a131b981e0f22536f84f2b43c52a2320d314d3e8
Joseba Urizarbarrena <jurizarbarrenahontoria@gmail.com>
2020-12-17 19:54:59 +0000

    Primer Commit

--- GoRestTest/NetworkManager.swift
+++ GoRestTest/NetworkManager.swift
@@ -7,10 +7,12 @@
 //
 
 import Foundation
+import Alamofire
 
 class Network {
     
     static var shared: Network = Network()
+    
     
 }
 
@@ -32,31 +34,66 @@ protocol GoRestAPI {
 
 extension Network: GoRestAPI {
     
-    static let ACCESS_TOKEN = "*****"
+    static let ACCESS_TOKEN =
+    "8638ff0993002cacd12015f9903b7d6e8b2387cb99c3a6d5045388a6eb287f5b"
+   
     
     //TODO
     func getUsers() {
-        <#code#>
+        
+        let url = URL(string: "https://gorest.co.in/public-api/posts")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
+        
+        AF.request("https://gorest.co.in/public-api/posts",method: .get,encoding: URLEncoding(destination: .queryString)).response { response in
+            debugPrint(response)
+        }
     }
     
     //TODO
     func postUser() {
-        <#code#>
+        
+        struct Login: Encodable{
+            
+            let token: String
+            
+        }
+        
+        let create = Login(token: "8638ff0993002cacd12015f9903b7d6e8b2387cb99c3a6d5045388a6eb287f5b")
+        
+        
+        let url = URL(string: "https://gorest.co.in/public-api/users")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
+        
+        AF.request("https://gorest.co.in/public-api/users",method: .post,parameters: create).response { response in
+        debugPrint(response)
+        }
     }
     
     //TODO
     func patchUser() {
-        <#code#>
+        let url = URL(string: "https://gorest.co.in/public-api/comments")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
+        
+        AF.request("https://gorest.co.in/public-api/comments",method: .patch).response { response in
+        debugPrint(response)
+        }
     }
     
     //TODO
     func putUser() {
-        <#code#>
+        let url = URL(string: "https://gorest.co.in/public-api/todos")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
+        
+        AF.request("https://gorest.co.in/public-api/todos",method: .put).response { response in
+        debugPrint(response)
+        }
+
     }
     
     //TODO
     func deleteUser() {
-        <#code#>
+        let url = URL(string: "https://gorest.co.in/public-api/posts")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
+        
+        AF.request("https://gorest.co.in/public-api/categories",method: .delete).response { response in
+        debugPrint(response)
+        }
+
     }
     
 }
