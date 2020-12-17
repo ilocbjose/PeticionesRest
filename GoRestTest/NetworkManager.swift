e7b90f63173fc5e68d9f11aa62189dcca3deaa47
Joseba Urizarbarrena <jurizarbarrenahontoria@gmail.com>
2020-12-17 20:11:59 +0000

    todo guay

--- GoRestTest/NetworkManager.swift
+++ GoRestTest/NetworkManager.swift
@@ -41,9 +41,15 @@ extension Network: GoRestAPI {
     //TODO
     func getUsers() {
         
-        let url = URL(string: "https://gorest.co.in/public-api/posts")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
+        let URLUser = "https://gorest.co.in/public-api/posts?acces-token=" + Network.ACCESS_TOKEN
         
-        AF.request("https://gorest.co.in/public-api/posts",method: .get,encoding: URLEncoding(destination: .queryString)).response { response in
+        URLSession.shared.dataTask(with: URL(string: URLUser)!){
+            data,response,error in
+            
+            debugPrint(response)
+        }.resume()
+        
+        AF.request("https://gorest.co.in/public-api/posts?acces-token=" + Network.ACCESS_TOKEN ,method: .get).response { response in
             debugPrint(response)
         }
     }
@@ -51,36 +57,48 @@ extension Network: GoRestAPI {
     //TODO
     func postUser() {
         
-        struct Login: Encodable{
+        let URLUser = "https://gorest.co.in/public-api/users?acces-token=" + Network.ACCESS_TOKEN
+        
+        URLSession.shared.dataTask(with: URL(string: URLUser)!){
+            data,response,error in
             
-            let token: String
-            
-        }
-        
-        let create = Login(token: "8638ff0993002cacd12015f9903b7d6e8b2387cb99c3a6d5045388a6eb287f5b")
+            debugPrint(response)
+        }.resume()
         
         
-        let url = URL(string: "https://gorest.co.in/public-api/users")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
-        
-        AF.request("https://gorest.co.in/public-api/users",method: .post,parameters: create).response { response in
+        AF.request("https://gorest.co.in/public-api/users?acces-token=" + Network.ACCESS_TOKEN ,method: .post).response { response in
         debugPrint(response)
         }
     }
     
     //TODO
     func patchUser() {
-        let url = URL(string: "https://gorest.co.in/public-api/comments")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
         
-        AF.request("https://gorest.co.in/public-api/comments",method: .patch).response { response in
+        let URLUser = "https://gorest.co.in/public-api/comments?acces-token=" + Network.ACCESS_TOKEN
+        
+        URLSession.shared.dataTask(with: URL(string: URLUser)!){
+            data,response,error in
+            
+            debugPrint(response)
+        }.resume()
+        
+        AF.request("https://gorest.co.in/public-api/comments?acces-token=" + Network.ACCESS_TOKEN,method: .patch).response { response in
         debugPrint(response)
         }
     }
     
     //TODO
     func putUser() {
-        let url = URL(string: "https://gorest.co.in/public-api/todos")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
         
-        AF.request("https://gorest.co.in/public-api/todos",method: .put).response { response in
+        let URLUser = "https://gorest.co.in/public-api/todos?acces-token=" + Network.ACCESS_TOKEN
+        
+        URLSession.shared.dataTask(with: URL(string: URLUser)!){
+            data,response,error in
+            
+            debugPrint(response)
+        }.resume()
+        
+        AF.request("https://gorest.co.in/public-api/todos?acces-token=" + Network.ACCESS_TOKEN,method: .put).response { response in
         debugPrint(response)
         }
 
@@ -88,9 +106,16 @@ extension Network: GoRestAPI {
     
     //TODO
     func deleteUser() {
-        let url = URL(string: "https://gorest.co.in/public-api/posts")!; let task = URLSession.shared.dataTask(with: url) {(data, response, error) in guard let data = data else { return }; print(String(data: data, encoding: .utf8)!) }; task.resume()
         
-        AF.request("https://gorest.co.in/public-api/categories",method: .delete).response { response in
+        let URLUser = "https://gorest.co.in/public-api/categories?acces-token=" + Network.ACCESS_TOKEN
+        
+        URLSession.shared.dataTask(with: URL(string: URLUser)!){
+            data,response,error in
+            
+            debugPrint(response)
+        }.resume()
+        
+        AF.request("https://gorest.co.in/public-api/categories?acces-token=" + Network.ACCESS_TOKEN,method: .delete).response { response in
         debugPrint(response)
         }
 
